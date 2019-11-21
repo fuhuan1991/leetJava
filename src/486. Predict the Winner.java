edd3f90d167e8311e9abaf34f2b400ee70b9a51c
fuhuan1991 <huanfu2019@gmail.com>
2019-11-21 11:05:09 -0500

    486. Predict the Winner

---
+++ src/486. Predict the Winner.java
@@ -0,0 +1,22 @@
+class Solution486 {
+  public boolean PredictTheWinner(int[] nums) {
+    int len = nums.length;
+    int[][] dp = new int[len][len];
+    // dp[s][e] stores the maximum advantage(difference) that a player can build from the
+    // current subArray(starts at index s, ends at index e).
+    for (int start = len-1; start >= 0; start--) {
+      dp[start][start] = nums[start];
+      for (int end = start + 1; end <= len-1; end++) {
+        int a = nums[start] - dp[start+1][end];
+        int b = nums[end] - dp[start][end-1];
+        dp[start][end] = Math.max(a, b);
+      }
+    }
+
+    if (dp[0][len-1] >= 0) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+}
