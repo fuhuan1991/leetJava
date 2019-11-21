2282e7058607946a07191b8586dc98b9fc3a5f49
fuhuan1991 <huanfu2019@gmail.com>
2019-11-21 11:09:18 -0500

    486. Predict the Winner

--- src/486. Predict the Winner.java
+++ src/486. Predict the Winner.java
@@ -7,6 +7,9 @@ class Solution486 {
     for (int start = len-1; start >= 0; start--) {
       dp[start][start] = nums[start];
       for (int end = start + 1; end <= len-1; end++) {
+        // once a play choose an end point, a subArray(start+1->end or start->end-1)
+        // is left for the other player. Then, the other player can use this subArray
+        // to build the maximum advantage.
         int a = nums[start] - dp[start+1][end];
         int b = nums[end] - dp[start][end-1];
         dp[start][end] = Math.max(a, b);
