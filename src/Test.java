770b5222a0a820c343c2dff32c7f1b2c8dc17c88
fuhuan1991 <huanfu2019@gmail.com>
2020-09-05 19:07:20 -0400

    480. Sliding Window Median

--- src/Test.java
+++ src/Test.java
@@ -4,14 +4,9 @@ import java.util.*;
 class Test {
 
   public static void main(String[] args) {
-//    Solution331 obj = new Solution331();
-//    System.out.println(obj.isValidSerialization("1,2,#,4,#,5,#,#,3,6,#,#,#"));
-    Object o1 = new Object();
-    Object o2 = new Object();
+    Solution973 obj = new Solution973();
+//    obj.partition()
 
-    System.out.println(o1.hashCode());
-    System.out.println(o2.hashCode());
-    System.out.println(o2.equals(o1));
 
   }
 
